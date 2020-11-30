8051938df5b60045c20cdc5092367f4c28aa6d3c
svetlana_gubaydullina <gubajdullina_svetlana@mail.ru>
2020-11-30 23:14:48 +0300

    Сделан новый класс GoogleCalc, все данные локаторов перенесены в этот класс.

--- src/main/java/pages/GoogleCalc.java
+++ src/main/java/pages/GoogleCalc.java
@@ -1,4 +1,91 @@
 package pages;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+import static org.openqa.selenium.support.PageFactory.initElements;
+
 public class GoogleCalc {
+
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td/div/div")
+    private WebElement q1;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td/div/div")
+    private WebElement q2;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[4]/div/div")
+    private WebElement q3;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[2]/div/div")
+    private WebElement q4;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")
+    private WebElement q5;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[4]/div/div")
+    private WebElement q6;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[3]/div/div")
+    private WebElement q7;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[4]/div/div")
+    private WebElement q8;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td/div/div")
+    private WebElement q9;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")
+    private WebElement q10;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")
+    private WebElement q11;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[2]/div/div")
+    private WebElement q12;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")
+    private WebElement q13;
+
+
+        @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[3]/div/div")
+        private WebElement p1;
+        @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")
+        private WebElement p2;
+        @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")
+        private WebElement p3;
+        @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")
+        private WebElement p4;
+
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table/tbody/tr[2]/td[2]/div/div")
+    private WebElement a1;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")
+    private WebElement a2;
+    @FindBy (xpath ="//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")
+    private WebElement a3;
+
+
+
+    public GoogleCalc(WebDriver driver){
+        initElements(driver, this);
+
+    }
+    public void googleCalcTestCase1(){
+        q1.click();
+        q2.click();
+        q3.click();
+        q4.click();
+        q5.click();
+        q6.click();
+        q7.click();
+        q8.click();
+        q9.click();
+        q10.click();
+        q11.click();
+        q12.click();
+        q13.click();
+
+    }
+        public void googleCalcTestCase2(){
+            p1.click();
+            p2.click();
+            p3.click();
+            p4.click();
+        }
+
+    public void googleCalcTestCase3(){
+        a1.click();
+        a2.click();
+        a3.click();
+
+    }
 }
--- src/test/java/tests/googleTests.java
+++ src/test/java/tests/googleTests.java
@@ -1,22 +1,23 @@
 package tests;
-//import java.util.regex.Pattern;
 
-import org.junit.jupiter.api.*;
-import org.openqa.selenium.*;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-
 import java.util.concurrent.TimeUnit;
-import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
-//import org.junit.*;
-//import static org.junit.Assert.*;
-//import static org.hamcrest.CoreMatchers.*;
+import pages.GoogleCalc;
+
 
 public class googleTests {
     private static WebDriver driver;
     private static String baseUrl;
-
+    private static GoogleCalc googleCalc;
 
     @BeforeEach
     public void setUp(){
@@ -27,6 +28,8 @@ public class googleTests {
         baseUrl = "http://www.google.ru/";
         driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
         driver.get("http://www.google.ru/");
+        googleCalc = new GoogleCalc(driver);
+
     }
 
     @Test
@@ -34,19 +37,20 @@ public class googleTests {
     public void testKeys1() {
         driver.findElement(By.name("q")).sendKeys("Calc");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[4]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[2]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[4]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[3]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[4]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[2]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+       // driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td/div/div")).click();
+     //   driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[4]/div/div")).click();
+    //    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[2]/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[4]/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[3]/div/div")).click();
+    //    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[4]/td[4]/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td/div/div")).click();
+   //     driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")).click();
+    //    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")).click();
+    //    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[2]/div/div")).click();
+  //      driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+        googleCalc.googleCalcTestCase1();
         assertEquals("1",driver.findElement(By.cssSelector("span[jsname='VssY5c']")).getText());
 
     }
@@ -56,10 +60,11 @@ public class googleTests {
     public void testKeys2(){
         driver.findElement(By.name("q")).sendKeys("Calc");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[3]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+        //driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[3]/td[3]/div/div")).click();
+      //  driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[2]/td[4]/div/div")).click();
+     //   driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td/div/div")).click();
+    //    driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+       googleCalc.googleCalcTestCase2();
         assertEquals("Infinity",driver.findElement(By.cssSelector("span[jsname='VssY5c']")).getText());
 
     }
@@ -68,9 +73,10 @@ public class googleTests {
     public void testKeys3() {
         driver.findElement(By.name("q")).sendKeys("Calc");
         driver.findElement(By.name("q")).sendKeys(Keys.ENTER);
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table/tbody/tr[2]/td[2]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")).click();
-        driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+      //  driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table/tbody/tr[2]/td[2]/div/div")).click();
+     //   driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr/td[2]/div/div")).click();
+     //   driver.findElement(By.xpath("//div[@id='rso']/div/div/div/div/div/div[3]/div/table[2]/tbody/tr[5]/td[3]/div/div")).click();
+       googleCalc.googleCalcTestCase3();
         assertEquals("Error",driver.findElement(By.cssSelector("span[jsname='VssY5c']")).getText());
             }
 
